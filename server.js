017d4b6df7823440544d69610545c3c7939e43af
sheshadrN <sdri9518@gmail.com>
2024-04-04 22:12:12 +0530

    sql

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 2000
+import express from "express";
+import mysql from "mysql";
+import morgan from "morgan";
 
-app.post('/signup',(req,res)=>{
-    const {username,password} = req.body;
-    if(!username||!password)
-    {
-        res.json({message:"all feilds required"});
+const App = express();
+App.use(express.json());
+App.use(morgan("dev"));
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "Shesha@007",
+  database: "shesha",
+});
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err.stack);
+    return;
+  }
+  console.log("Connected to MySQL server");
+  const create_table = `CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY Key,
+            username varChar(30) not null,
+            email varChar(20) not null,
+            password varChar(255) not null)`;
+  connection.query(create_table, (err, result) => {
+    if (err) {
+      console.log(err.message);
     }
-    res.json({message:"registered successfully"});
-})
+    console.log("database is created successfully");
+  });
+});
 
+App.post('/registerOtp',registerOTP);
+const registerOTP=()=>{
+  
+}
 
-app.listen(port,()=>{
-    console.log(`server is running`)
-})
+App.listen(3000, () => {
+  console.log(`server is running http://localhost:3000`);
+});
