49fd6c73de06d7797b69156ce05854f28795f692
sheshadrN <sdri9518@gmail.com>
2024-04-07 11:22:49 +0530

    removed console

--- functions/password.js
+++ functions/password.js
@@ -4,9 +4,7 @@ import bcrypt, { hash } from "bcrypt";
 const hash_password = (password) => {
   try {
     const salt = bcrypt.genSaltSync(Number(process.env.SALT));
-
     const hashed = bcrypt.hashSync(password, salt);
-    console.log(salt + "==>> " + hashed);
     return hashed;
   } catch (error) {
     console.error(error);
